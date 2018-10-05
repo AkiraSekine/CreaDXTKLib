ec110568ea9a2ef2cb67f158c8bfd0070d36f287
AkiraSekine <ekusika834@gmail.com>
2018-10-05 18:38:18 +0900

    add source files

---
+++ src/Draw/Image.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#ifdef _DEBUG
+#pragma comment(lib, "Draw_d.lib")
+#else
+#pragma comment(lib, "Draw.lib")
+#endif // _DEBUG
+
+
+namespace CreaDXTKLib
+{
+namespace Draw
+{
+    class Image final
+    {
+    private:
+
+    public:
+
+    };
+} // Utility
+} // CreaDXTKLib
