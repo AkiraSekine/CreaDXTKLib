95bdc992f198bba45db739f6e2e54ad9d015bdb9
AkiraSekine <ekusika834@gmail.com>
2018-10-14 18:36:39 +0900

    bug fixed

--- src/CreaDXTKLib/GameManager.cpp
+++ src/CreaDXTKLib/GameManager.cpp
@@ -13,7 +13,7 @@ namespace CreaDXTKLib
 {
     void GameManager::Initialize(ComPtr<ID3D11DeviceContext1> _context, ComPtr<ID3D11Device1>& _device)
     {
-        Image::Initialize(_context, _device);
+        Image::Instance().Initialize(_context, _device);
         Keyboard::Initialize();
 
         SetUp();
